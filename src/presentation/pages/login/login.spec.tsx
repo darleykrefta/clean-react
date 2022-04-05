a406a1ed87cd6cb071f931b4b6768abab42de0d7
Darley Krefta <darleykrefta@hotmail.com>
2022-04-04 21:59:44 -0300

    test: added initial status state to login form

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -11,5 +11,10 @@ describe('Login component', () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = screen.getByText('Entrar') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = screen.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    const passwordStatus = screen.getByTestId('password-status')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
