2164ded2c8b8e97332b8a5036a9adbeef0a55121
Darley Krefta <darleykrefta@hotmail.com>
2022-05-23 23:30:51 -0300

    test: changed validation interface

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -11,11 +11,13 @@ type SutTypes = {
 }
 
 class ValidationSpy implements Validation {
+  fieldName: string
+  fieldValue: string
   errorMessage: string
-  input: object
 
-  validate(input: object): string {
-    this.input = input
+  validate(fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName
+    this.fieldValue = fieldValue
     return this.errorMessage
   }
 }
@@ -49,19 +51,15 @@ describe('Login component', () => {
     const { validationSpy } = makeSut()
     const emailInput = screen.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: 'any_email' } })
-    expect(validationSpy.input).toEqual({
-      email: 'any_email',
-      password: ''
-    })
+    expect(validationSpy.fieldName).toEqual('email')
+    expect(validationSpy.fieldValue).toEqual('any_email')
   })
 
   it('Should call validation with correct password', () => {
     const { validationSpy } = makeSut()
     const passwordInput = screen.getByTestId('password')
     fireEvent.input(passwordInput, { target: { value: 'any_password' } })
-    expect(validationSpy.input).toEqual({
-      email: '',
-      password: 'any_password'
-    })
+    expect(validationSpy.fieldName).toEqual('password')
+    expect(validationSpy.fieldValue).toEqual('any_password')
   })
 })
